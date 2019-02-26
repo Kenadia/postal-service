a9cdc4b3a29484152fcb3cee8e29b65f3b319ead
Kenneth Schiller <kenschiller@gmail.com>
2019-02-26 03:15:56 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="postal-service",
+    version="0.0.1",
+    author="Kenneth Schiller",
+    author_email="kenschiller@gmail.com",
+    description="An email server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kenadia/postal-service",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+)
